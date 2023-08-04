3a51ef539b3212207c447b02bea83dbb578db297
sandy13oct1992 <sandy13oct1992@gmail.com>
2023-08-04 13:29:28 +0530

    get element by class name

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 
-//const elements = document.getElementsByClassName("item");
-//elements[0].style.backgroundColor='green';
+const elements = document.getElementsByClassName("item");
+elements[2].style.backgroundColor='green';
 //elements[1].style.backgroundColor='yellow';
+
+for(var i=0; i<elements.length;i++)
+{
+  elements[i].style.fontweight='bold';
+}
 
 //const elements = document.getElementsById("item"
 
-const name = document.getElementById("name");
-const email = document.getElementById("E-mail");
-const button = document.getElementById("Submit");
+//const name = document.getElementById("name");
+//const email = document.getElementById("E-mail");
+//const button = document.getElementById("Submit");
 
-const submitFunction = (event) =>{
-    event.preventDefault();
-    console.log(name.value)
-    console.log(email.value)
-    }
-button.addEventListener("click",submitFunction);
+//const submitFunction = (event) =>{
+  //  event.preventDefault();
+    //console.log(name.value)
+    //console.log(email.value)
+   // }
+//button.addEventListener("click",submitFunction);
