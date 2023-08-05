0abe86af535aacbd7e6ff795262c5b1e978c858d
sandy13oct1992 <sandy13oct1992@gmail.com>
2023-08-05 23:16:16 +0530

    made background color green of 2nd item

--- main.js
+++ main.js
@@ -20,12 +20,12 @@
     //console.log(email.value)
    // }
 //button.addEventListener("click",submitFunction);
-const li = document.getElementsByTagName("li");
-console.log(li);
-li[2].style.backgroundColor='green';
+//const li = document.getElementsByTagName("li");
+//console.log(li);
+//li[2].style.backgroundColor='green';
 //elements[1].style.backgroundColor='yellow';
 
-for(var i=0; i<li.length;i++)
-{
-  li[i].style.fontweight='bold';
-}
+//for(var i=0; i<li.length;i++)
+//{
+  //li[i].style.fontweight='bold';
+//}
