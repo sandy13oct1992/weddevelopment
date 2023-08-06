df24848788faa624970b7a15d63fcef2da4907fc
sandy13oct1992 <sandy13oct1992@gmail.com>
2023-08-06 11:19:54 +0530

    creating node and modifying dom

--- main.js
+++ main.js
@@ -29,5 +29,57 @@
 //{
   //li[i].style.fontweight='bold';
 //}
-var seconditem=document.querySelector('.list-group-item:nth-child(2)');
-seconditem.style.backgroundcolor='green';
+//var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+//seconditem.style.backgroundcolor='green';
+
+//parent node
+//var itemlist=document.querrySelector('#items');
+//console.log(itemlist.parentNode);
+
+//last child
+//console.log(itemlist.lastChild);
+//last Eelement child
+//console.log(itemlist.lastElementChild);
+
+//first child
+//console.log(itemlist.firstChild);
+//last Eelement child
+//console.log(itemlist.firstElementChild);
+
+//Next Sibling
+//console.log(itemlist.nextSibling);
+//next element sibling
+//console.log(itemlist.nextElementSibling);
+
+//previous Sibling
+//console.log(itemlist.previousSibling);
+//previous Element sibling
+//console.log(itemlist.previousElementSibling);
+
+//create new element
+ 
+//create new div
+var newdiv = document.createElement('div');
+
+//add class name
+newdiv.className= 'hello';
+
+//add id
+newdiv.id='hello1';
+
+//add attribute
+newdiv.setAttribute('title','hello div');
+
+//create textNode
+var newdivTN=newdiv.creatTextNode('hello world');
+
+newdiv.append(newdivTN);
+
+var container=document.querrySelector('header .container');
+
+var h1=document.querrySelector('header h1');
+
+console.log(newdiv);
+
+container.insertBefore(newdiv, h1);
+
